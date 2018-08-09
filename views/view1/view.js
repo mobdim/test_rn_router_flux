910127728026de89dcdddcd3fc59059f8236129f
mobdim <dima@mobdim.com>
2018-08-09 15:26:34 +0300

    version beta.42

--- views/view1/view.js
+++ views/view1/view.js
@@ -6,6 +6,7 @@ import {
   View, Text, TouchableOpacity
 } from 'react-native'
 
+import { Actions } from 'react-native-router-flux'
 
 export default class View1 extends Component {
   componentWillMount() {
@@ -14,18 +15,20 @@ export default class View1 extends Component {
       onAppStart,
     } = this.props
 
-    onAppStart()
+    //onAppStart()
+    Actions.view2()
   }
 
   render() {
     const {
       onAppStart,
     } = this.props
+
     return (
       <View>
         <Text>View1</Text>
         <TouchableOpacity onPress={() => onAppStart()}>
-          <Text style={{color: 'blue'}}>go to View2</Text>
+          <Text style={{ color: 'blue' }}>go to View2</Text>
         </TouchableOpacity>
       </View>
     )
